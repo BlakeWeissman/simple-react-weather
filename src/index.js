b92829b7ce8e028f43b8e8580514fe8acbfae6b1
Blake Weissman <email@blakeweissman.com>
2019-04-15 21:04:14 -0400

    Created basic app structure

--- src/index.js
+++ src/index.js
@@ -1,12 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import Result from './result';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+export class Index extends Component {
+  constructor(props) {
+    super(props);
+    //this.search = this.search.bind(this);
+  }
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  /*search() {
+    const value = parseInt(document.getElementById("search").value);
+    if (isNaN(value) === false) {     
+      this.setState({price: parseInt(document.getElementById("filter-price").value)});
+    } 
+    else {
+      this.setState({price: 0});
+    }
+  }*/
+
+  render() {
+    return (
+      <div>
+        <div className="navbar">
+          <div className="wrap vCenter flex">
+            <div className="left">
+              <h2>A Simple React Weather App</h2><p>Created by Blake Weissman</p>
+            </div>
+            <div className="right vCenter">
+              <p>Search by Town/City Name</p>
+              <input id="search" onChange={this.search}></input>
+            </div>  
+          </div>
+        </div>
+        <Result />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<Index />, document.getElementById("root"));
