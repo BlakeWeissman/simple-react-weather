288b18ae9c36630337c7e9238c573459ffe496b7
Blake Weissman <email@blakeweissman.com>
2019-04-17 12:12:56 -0400

    Updates

---
+++ precache-manifest.3bfc37478d3a794d705e29c16b7a6283.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f33d3e57c7999cc8a837",
+    "url": "/simple-react-weather/static/js/runtime~main.3e340d08.js"
+  },
+  {
+    "revision": "d9ee1fb0d2ca76a4c174",
+    "url": "/simple-react-weather/static/js/main.f41f09f5.chunk.js"
+  },
+  {
+    "revision": "e8361ae5a4c725465aec",
+    "url": "/simple-react-weather/static/js/2.8fcae049.chunk.js"
+  },
+  {
+    "revision": "d9ee1fb0d2ca76a4c174",
+    "url": "/simple-react-weather/static/css/main.b9a5648d.chunk.css"
+  },
+  {
+    "revision": "30c537cd80a11400aac173e1d7060e30",
+    "url": "/simple-react-weather/index.html"
+  }
+];
